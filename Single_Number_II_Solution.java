1673a2372f0f44c95b9d83de38f959427a64445c
Abhishek Salaria <abhisheksalaria26@gmail.com>
2023-07-04 13:57:03 +0530

    Adding Solution To Single Number II LeetCode.

---
+++ Single_Number_II_Solution.java
@@ -0,0 +1,13 @@
+public class Single_Number_II_Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0;
+        int twos = 0;
+
+        for(int num: nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+
+        return ones;
+    }
+}
