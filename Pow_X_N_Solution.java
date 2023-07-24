9a97170fd66ef4e569b065d732e9256781eaf941
Abhishek Salaria <abhisheksalaria26@gmail.com>
2023-07-24 12:57:14 +0530

    Adding Solution To Pow(x,n) LeetCode.

---
+++ Pow_X_N_Solution.java
@@ -0,0 +1,16 @@
+public class Pow_X_N_Solution {
+    public double myPow(double x, long n) {
+        if(n == 0){
+            return 1;
+        }
+        if(n < 0) {
+            return 1.0 / myPow(x, -1 * n);
+        }
+        if(n % 2 == 0){
+            return myPow(x * x, n/2);
+        }
+        else {
+            return x * myPow(x * x, (n -1)/2);
+        }
+    }
+}
