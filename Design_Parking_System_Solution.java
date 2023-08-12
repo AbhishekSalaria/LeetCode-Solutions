9388f79cf17189c26e53d46517ca9de0622654ef
Abhishek Salaria <abhisheksalaria26@gmail.com>
2023-08-12 22:34:53 +0530

    Adding Solution To Design Parking System LeetCode.

---
+++ Design_Parking_System_Solution.java
@@ -0,0 +1,35 @@
+public class Design_Parking_System_Solution {
+    private int big;
+    private int medium;
+    private int small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+        if(carType == 1) {
+            if(big > 0) {
+                big -= 1;
+                return true;
+            }
+            return false;
+        }
+        else if(carType == 2) {
+            if(medium > 0) {
+                medium -= 1;
+                return true;
+            }
+            return false;
+        }
+        else {
+            if(small > 0) {
+                small -= 1;
+                return true;
+            }
+            return false;
+        }
+    }
+}
